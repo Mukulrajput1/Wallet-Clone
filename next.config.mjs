7eb7aa5a0465b3720458063ea78a3425da69d665
rana <rana.ranbir@dev.familyone.io>
2025-04-12 12:25:11 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
     env: {
         DOMAIN: process.env.DOMAIN,
-        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
         // Add more environment variables as needed
       }
 };
